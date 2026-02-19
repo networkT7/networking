a0730f6cb0b2c4a3fdd0d31ade45cccb28ddc0d3
Zachareee <zacharytanhx@yahoo.com>
2026-02-19 16:05:46 +0800

    optimise wire internals

--- src/networking/wire.py
+++ src/networking/wire.py
@@ -7,18 +7,18 @@ logger = create_logger(__name__)
 
 
 class Wire:
-    __map: dict[str, socket.SocketType]
+    __targets: list[socket.SocketType]
 
     def _broadcast(self, msg: bytes):
-        for sock in self.__map.values():
+        for sock in self.__targets:
             sock.send(msg)
 
     def forward(self):
-        for sock in self.__map.values():
+        for sock in self.__targets:
             sock.settimeout(1)
 
         while True:
-            for sock in self.__map.values():
+            for sock in self.__targets:
                 try:
                     msg = sock.recv(1000)
                     logger.info(f"sending message {msg}")
@@ -28,10 +28,10 @@ class Wire:
 
     def __init__(self, connected_nodes: list[str],
                  nodes: dict[str, NodeConfig]):
-        self.__map = {node: socket.create_connection(
-            ("127.0.0.1", nodes[node]["port"])) for node in connected_nodes}
+        self.__targets = [socket.create_connection(
+            ("127.0.0.1", nodes[node]["port"])) for node in connected_nodes]
 
     def __del__(self):
         logger.info("closing sockets")
-        for v in self.__map.values():
+        for v in self.__targets:
             v.close()
