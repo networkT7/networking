278b73b66018f1465e72a3729f5afbc9e03e1484
Zachareee <zacharytanhx@yahoo.com>
2026-03-20 01:12:10 +0800

    Use different session name

--- start.bat
+++ start.bat
@@ -1,23 +1,23 @@
-tmux new -d -n node1 -c .
+tmux new -d -s networking
 tmux neww
 tmux send "task -- wire W1" ENTER
 tmux splitw -h
 tmux send "task -- wire W2" ENTER
 tmux splitw -v
 tmux send "task -- wire W3" ENTER
-tmux selectp -t default:1.0
+tmux selectp -t networking:1.0
 tmux splitw -v
 tmux send "task -- router" ENTER
 
-tmux selectw -t default:0
+tmux selectw -t networking:0
 tmux send "task -- node N1" ENTER
 tmux splitw -h
 tmux send "task -- node N2" ENTER
 tmux splitw -v
 tmux send "task -- node N4" ENTER
-tmux selectp -t default:0.0
+tmux selectp -t networking:0.0
 tmux splitw -v
 tmux send "task -- node N3" ENTER
 
 
-tmux attach -t default
+tmux attach -t networking
